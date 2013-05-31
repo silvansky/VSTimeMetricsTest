0bdd8b39d744fb9f4df97e91c77d271d66b69788
silvansky <v.silvansky@gmail.com>
2013-05-31 14:53:20 +0400

    Initial Commit

---
+++ TimeMetricsTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeMetricsTest
+//
+//  Created by Valentine Gorshkov on 31.05.13.
+//  Copyright (c) 2013 silvansky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
