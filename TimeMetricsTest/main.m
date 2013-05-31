f0e4f1b74b130cf332b1bcbaa2fe141c2956d333
silvansky <v.silvansky@gmail.com>
2013-05-31 15:11:17 +0400

    initial commit

--- TimeMetricsTest/main.m
+++ TimeMetricsTest/main.m
@@ -7,16 +7,48 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "VSTimeMetrics.h"
+
+#define NUM_THREADS   4
 
 int main(int argc, const char * argv[])
 {
-
-	@autoreleasepool {
-	    
-	    // insert code here...
-	    NSLog(@"Hello, World!");
-	    
+	@autoreleasepool
+	{
+		dispatch_queue_t queues[NUM_THREADS];
+		dispatch_group_t group = dispatch_group_create();
+		for (int i = 0; i < NUM_THREADS; i++)
+		{
+			queues[i] = dispatch_queue_create("async queue", DISPATCH_QUEUE_CONCURRENT);
+		}
+		[[VSTimeMetrics sharedInstance] startMeasuringForKey:@"total"];
+		int currentQueue = 0;
+		for (int i = 1; i < 100; i++)
+		{
+			dispatch_block_t block = ^{
+				@autoreleasepool
+				{
+					[[VSTimeMetrics sharedInstance] startMeasuringForKey:@"array"];
+					NSMutableArray *a = [NSMutableArray arrayWithCapacity:i * i];
+					for (int j = 0; j < i * i; j++)
+					{
+						[a addObject:@(j)];
+					}
+					[[VSTimeMetrics sharedInstance] finishMeasuringForKey:@"array"];
+				}
+			};
+			dispatch_group_async(group, queues[currentQueue], block);
+			currentQueue++;
+			if (currentQueue == NUM_THREADS)
+			{
+				currentQueue = 0;
+			}
+		}
+		dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+		[[VSTimeMetrics sharedInstance] finishMeasuringForKey:@"total"];
+		NSLog(@"array:\n\tlast: %f,\n\tavg: %f,\n\ttotal: %f", [[VSTimeMetrics sharedInstance] lastMeasurementForKey:@"array"], [[VSTimeMetrics sharedInstance] averageMeasurementForKey:@"array"], [[VSTimeMetrics sharedInstance] totalMeasurementForKey:@"array"]);
+		NSLog(@"total: %f", [[VSTimeMetrics sharedInstance] lastMeasurementForKey:@"array"]);
 	}
-    return 0;
+	return 0;
 }
 
